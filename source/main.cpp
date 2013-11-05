6166e431902efdcedca7966a5c40e7e2388af85d
Alex Mayfield <alexmax2742@gmail.com>
2013-11-05 18:37:23 -0500

    Use VBO's instead of immediate mode.

--- source/main.cpp
+++ source/main.cpp
@@ -49,6 +49,24 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
+	// Allocate a Vertex Array (why?)
+	GLuint vertexArrayID;
+	glGenVertexArrays(1, &vertexArrayID);
+	glBindVertexArray(vertexArrayID);
+
+	// Triangle data
+	const GLfloat triangle[9] = {
+		.0f, .75f, .0f,
+		.75f, -.75f, .0f,
+		-.75f, -.75f, .0f
+	};
+
+	// Pipe our vertex data into a VBO and hand it off to the video card
+	GLuint vertexBufferID;
+	glGenBuffers(1, &vertexBufferID);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+
 	for (;;) {
 		SDL_Event event;
 		while (SDL_PollEvent(&event)) {
@@ -60,11 +78,15 @@ int main(int argc, char** argv) {
 			}
 		}
 
-		glBegin(GL_TRIANGLES);
-			glVertex3f(0.0f, 0.0f, 0.0f);
-			glVertex3f(1.0f, 0.0f, 0.0f);
-			glVertex3f(1.0f, 1.0f, 0.0f);
-		glEnd();
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		// The actual draw call!
+		// It's a triangle, start at vertex 0 and draw all three vertexes.
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glDisableVertexAttribArray(0);
 
 		SDL_GL_SwapWindow(window);
 	}
